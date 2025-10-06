df262052ca0821075c15010a2bf62206631ebee8
MrDen4lk <OffEvstigneevDA@yandex.ru>
2025-10-06 22:42:32 +0300

    add models & fix infer_model.py

--- desktop/data_utils/infer_model.py
+++ desktop/data_utils/infer_model.py
@@ -1,24 +1,55 @@
+import json
 import pandas as pd
 import numpy as np
 import joblib as jl
 
+import config
+from training import change_mapping, uniq_ration, uniq_step, uniq_changed_ration, name_mapping
 
-def predict_from_file(model_path: str, input_csv: str, output_csv: str):
-    """
-    Аргументы:
-        model_path: путь к файлу модели (.pkl)
-        input_csv: CSV с признаками (без столбца таргета)
-        output_csv: путь для сохранения CSV с предсказаниями
-    """
-    model = jl.load(model_path)
-    print(f"Модель загружена: {model_path}")
 
-    data = pd.read_csv(input_csv)
-    print(f"Загружено {len(data)} строк из {input_csv}")
+def extract_to_row(ingridients):
+    row = [0] * (len(uniq_changed_ration) + len(uniq_step))  # todo: тут жесткий костыль
+    uniq_dict = {elem: ind for ind, elem in enumerate(uniq_changed_ration)}
+    name_mapping = change_mapping()
 
-    preds = model.predict(data)
+    for elem in ingridients:  # todo: изменить когда в json будет храниться чисто 2 элемента
+        clear_name = name_mapping[elem[0]]  # todo: сделать здесь проверку есть ли элемент в name_mapping если нет то ллм прикрутить
+        row[uniq_dict[clear_name]] += float(elem[-1].replace(',', '.'))
 
-    data["prediction"] = preds
+    return row
 
-    data.to_csv(output_csv, index=False)
-    print(f"Результаты сохранены в {output_csv}")
+def load_data_from_json(path_name: str):
+    with open(path_name, "r", encoding="utf-8") as f:
+        json_file = json.load(f)
+    rows = [list(elem.values()) for elem in json_file['rows']]
+
+    final_row = extract_to_row(rows)
+    return final_row
+
+def clear_data(data):
+    for item in config.for_dropping:
+        data = data.drop(item, axis=1)
+
+    for key, value in config.medians_of_data:
+        if data[key] is None:
+            data[key] = value
+
+    return data.to_numpy()
+
+def predict_from_file(json_report, model_path, acids_list):
+    acids_dict = dict()
+
+    data = load_data_from_json(json_report)
+    data = clear_data(data)
+
+    for acid in acids_list:
+        model = jl.load(f"{model_path}/{acid}/_ensemble.pkl")
+        logit = model.predict(data)
+        acids_dict[acid] = logit
+
+    return acids_dict
+
+if __name__ == '__main__':
+    print(predict_from_file(json_report="../reports/report_2025-10-05_1759679897.json",
+                            model_path="../../models/classic_pipe",
+                            acids_list=config.acids))
